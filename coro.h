2798cd5d9407c97b766c75a365b468702451c89c
u66u <cake3327@gmail.com>
2025-11-23 18:10:31 +0300

    macro for running coroutines

--- coro.h
+++ coro.h
@@ -4,15 +4,13 @@
 #include "arena.h"
 #include <ucontext.h>
 
+
 typedef struct {
     ucontext_t context;
     void* stack;
-    enum { CORO_RUNNABLE, 
-           CORO_WAITING, 
-           CORO_DEAD,
-           CORO_READY
-     } status;
+    enum { CORO_RUNNABLE, CORO_WAITING, CORO_DEAD, CORO_READY } status;
 } coroutine_t;
+
 
 coroutine_t* coro_create(void (*func)(void*), void* arg, stack_pool_t* pool) {
     if (!pool) {
